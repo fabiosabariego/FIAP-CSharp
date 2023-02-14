aaf020a06804f8786e53d7e9dbfe2b11ebf432c0
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-02-13 22:15:18 -0300

    Primeiro Exercicio

--- 01 - Logicas Programacao/Ex01/Program.cs
+++ 01 - Logicas Programacao/Ex01/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Converter um valor recebido em dolar para real
+            float dolar;
+            float real;
+
+            // Dolar 5,34
+            Console.Write("Digite o valor em dolar: U$");
+            float.TryParse(Console.ReadLine(), out dolar);
+
+            real = (dolar * 5.34f);
+
+            Console.WriteLine($"Valor em dolar: U${dolar} convertido em Reais: R${Math.Round(real, 2)}");
+            
         }
     }
 }
