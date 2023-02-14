68aa0fa7caec740ecd74cdbcd6b2baf3908746e3
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-02-13 21:18:10 -0300

    Aula e exercicios Exemplo

--- 01 - Logicas Programacao/Aula01/Program.cs
+++ 01 - Logicas Programacao/Aula01/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int filhos = (int) 3.0; // Casting
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine($"Caramba, voce tem {filhos} filhos?");
+            //Console.WriteLine("");
+
+            int n1, n2, res;
+            float resFloat;
+
+            Console.Write("Digite numero 01: ");
+            //int.TryParse(Console.ReadLine(), out n1);     Esta e uma opcao para fazer a conversao
+            n1 = Convert.ToInt16(Console.ReadLine());       // Esta e uma outra forma de fazer a convesao
+
+            Console.Write("Digite numero 02: ");
+            int.TryParse(Console.ReadLine(), out n2);
+
+            Console.WriteLine($"{n1} + {n2} = {res = n1 + n2}");
+            Console.WriteLine($"{n1} - {n2} = {res = n1 - n2}");
+            Console.WriteLine($"{n1} * {n2} = {res = n1 * n2}");
+            Console.WriteLine($"{n1} / {n2} = {resFloat = (float)n1 / n2}");
+
         }
     }
 }
