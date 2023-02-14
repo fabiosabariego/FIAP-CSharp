9cb6c625f9b7c7dccea51cfd9eaa3095fbf9d849
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-02-14 20:09:21 -0300

    Aulas Condicionais (IF)

---
+++ 01 - Logicas Programacao/Aula02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml;
+
+namespace Aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores Relacionais e condicoes (IF)
+
+            int idade;
+
+            Console.Write("Digite sua idade: ");
+            int.TryParse(Console.ReadLine(), out idade);
+
+            if (idade > 12)
+            {
+                Console.WriteLine("Voce pode jogar!!");
+            }
+            else if (idade == 12)
+            {
+                Console.WriteLine("A partir dessa idade voce pode jogar");
+            }
+            else
+            {
+                Console.WriteLine("Voce nao tem idade para jogar!!");
+            }
+
+        }
+    }
+}
---
+++ 01 - Logicas Programacao/Exercicio/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valorDoado;
+            double investimento;
+
+            Console.Write("Digite o valor a ser doado: R$");
+            float.TryParse(Console.ReadLine(), out valorDoado);
+
+            if (valorDoado >= 1000.00)
+            {
+                investimento = valorDoado * 0.15;
+            }
+            else
+            {
+                investimento = valorDoado * 0.05;
+            }
+
+            Console.WriteLine($"O valor doado foi de R${Convert.ToDouble(Math.Round(investimento,2))}");
+
+        }
+    }
+}
