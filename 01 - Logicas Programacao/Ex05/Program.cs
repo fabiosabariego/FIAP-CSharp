dc36d3d4a8ed4b75c039ddec227ec48719d7d071
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-02-14 21:54:18 -0300

    Exercicios Condicoes
    Exercicios de fixacao para condicoes if / Else

---
+++ 01 - Logicas Programacao/Ex05/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario;
+            double calcDesconto;
+
+            Console.Write("Digite seu Salario: R$");
+            float.TryParse(Console.ReadLine(), out salario);
+
+            calcDesconto = salario * 0.11;
+
+            if (calcDesconto >= 720.00)
+            {
+                calcDesconto = 720.00;
+            }
+
+            Console.WriteLine($"Com o Salario de R${Math.Round(Convert.ToDouble(salario), 2)} temos o desconto de R${Math.Round(Convert.ToDouble(calcDesconto), 2)}");
+
+        }
+    }
+}
