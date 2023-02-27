a2c5c94a5744e498777575552912bc70f5c6d118
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-02-27 20:17:06 -0300

    Aulas FOR

---
+++ 02 - FIAP_Console/Repeticao01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Repeticao01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tabuada
+
+            int num;
+            int res;
+
+            Console.Write("Digite numero para tabuada: ");
+            int.TryParse(Console.ReadLine(), out num);
+
+            for (int i = 1; i < 11; i++)
+            {
+                res = num * i;
+                Console.WriteLine($"{num} x {i} = {num * i}");
+            }
+            Console.WriteLine("Fim da Tabuada!!");
+
+        }
+    }
+}
