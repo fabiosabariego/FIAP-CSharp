f0254082d4141c487d38cc7e8a84cccfe60edb22
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-03-01 21:58:15 -0300

    Switch Case
    Aula usando Switch / Case

---
+++ 02 - FIAP_Console/AulasFIAP/CalcPesoAltura/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace CalcPesoAltura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string continuar;
+            int sexo;
+            double altura, peso;
+
+            do
+            {
+                Console.Write("Digite [1] - Homen / [2] - Mulher: ");
+                int.TryParse(Console.ReadLine(), out sexo);
+
+                Console.Write("Digite a Altura: ");
+                double.TryParse(Console.ReadLine(), out altura);
+
+                switch (sexo)
+                {
+                    case 1:
+                        peso = 72.2 * altura - 58;
+                        Console.WriteLine($"O Peso ideal para Homem e: {peso}");
+                        break;
+                    
+                    case 2:
+                        peso = 62.1 * altura - 44.7;
+                        Console.WriteLine($"O Peso ideal para Mulher e: {peso}");
+                        break;
+
+                    default:
+                        Console.Write("Valor informado nao existente!");
+                        break;
+                }
+
+                Console.WriteLine("");
+                Console.Write("Deseja Calcular Novamente? [Y/N]: ");
+                Console.WriteLine("");
+                continuar = Console.ReadLine();
+
+            } while (continuar.ToLower().Equals("y"));
+        }
+    }
+}
---
+++ 02 - FIAP_Console/AulasFIAP/ValorVenda/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ValorVenda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string continuar;
+            double valorVenda, calculoVenda = 0.0f;
+            int formaVenda;
+
+            do
+            {
+                Console.Write("Digite o Valor de Venda R$: ");
+                double.TryParse(Console.ReadLine(), out valorVenda);
+                Console.Write("Escolha forma Pagamento: [1] - A Vista / [2] - Debito / [3] - Cred. 3x (5%) / [4] - Cred. 4x+ (10%): ");
+                int.TryParse(Console.ReadLine(), out formaVenda);
+
+                switch (formaVenda)
+                {
+                    case 1:
+                        calculoVenda = valorVenda - (valorVenda * 0.1);
+                        break;
+                    case 2:
+                        calculoVenda = valorVenda;
+                        break;
+                    case 3:
+                        calculoVenda = valorVenda + (valorVenda * 0.05);
+                        break;
+                    case 4:
+                        calculoVenda = valorVenda + (valorVenda * 0.1);
+                        break;
+                    default:
+                        Console.WriteLine("Valor informado nao existente!");
+                        break;
+                }
+                Console.WriteLine($"Valor final da Compra R${Math.Round(calculoVenda, 2)}");
+
+                Console.WriteLine("");
+                Console.Write("Deseja Executar Novamente? [Y/N]: ");
+                continuar = Console.ReadLine();
+                Console.WriteLine("");
+
+            } while (continuar.ToLower().Equals("y"));
+        }
+    }
+}
