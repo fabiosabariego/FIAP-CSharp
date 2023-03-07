c8909bfd245a327cd3132f5b8ccb7cf536fe2bba
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-03-06 22:00:15 -0300

    Exercicios

--- 02 - FIAP_Console/AulasFIAP/CalcBaseExpoente/Program.cs
+++ 02 - FIAP_Console/AulasFIAP/CalcBaseExpoente/Program.cs
@@ -2,27 +2,28 @@
 {
     internal class Program
     {
-        public static double[] dados = new double[2];
 
         static void Main(string[] args)
         {
             SolValores();
-            CalcExp(dados[0], dados[1]);
         }
 
         public static void SolValores()
         {
+            double[] dados = new double[2];
             Console.Write("Digite o Valor da base: ");
             double.TryParse(Console.ReadLine(), out dados[0]);
 
             Console.Write("Digite o Valor do expoente: ");
             double.TryParse(Console.ReadLine(), out dados[1]);
+
+            CalcExp(dados[0], dados[1]);
         }
 
         public static void CalcExp(double baseVal, double exp)
         {
             double resCalc = 1;
-            for (int i = 1; i <= dados[1]; i++)
+            for (int i = 1; i <= exp; i++)
             {
                 resCalc *= baseVal;
             }
--- 02 - FIAP_Console/AulasFIAP/SolTemp/Program.cs
+++ 02 - FIAP_Console/AulasFIAP/SolTemp/Program.cs
@@ -5,7 +5,6 @@
         static void Main(string[] args)
         {
             lerTemp();
-
         }
 
         public static void lerTemp()
