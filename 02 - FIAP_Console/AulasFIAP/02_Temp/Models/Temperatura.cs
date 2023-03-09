41281549fdbb299c681431def5b2645790bedbae
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-03-09 06:34:05 -0300

    Aula POO

---
+++ 02 - FIAP_Console/AulasFIAP/02_Temp/Models/Temperatura.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Temp.Models
+{
+    internal class Temperatura
+    {
+        //Atributos
+        public double valor;
+
+        //Metodo Celsius
+        public double Celsius()
+        {
+            return (5 * valor - 160) / 9;
+        }
+
+        public double Fahrenheit()
+        {
+            return (9 * valor + 160) / 5;
+        }
+
+    }
+
+    
+}
---
+++ 02 - FIAP_Console/AulasFIAP/02_Temp/Program.cs
@@ -0,0 +1,18 @@
+﻿using _02_Temp.Models;
+
+namespace _02_Temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperatura temp = new Temperatura();
+
+            Console.Write("Digite um Valor: ");
+            double.TryParse(Console.ReadLine(), out temp.valor);
+
+            Console.WriteLine($"Conversao da Temperatura em Celsius: {temp.Celsius()}");
+            Console.WriteLine($"Conversao da Temperatura em Fahrenheit: {temp.Fahrenheit()}");
+        }
+    }
+}
