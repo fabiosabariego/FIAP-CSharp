a8ce85ad8065ebc30185b60c4a76181e10a97125
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-03-20 19:48:07 -0300

    Polimorfismo

---
+++ 02 - FIAP_Console/AulasFIAP/03_Veiculos/Models/Segurado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Veiculos.Models
+{
+    internal class Segurado : Veiculo
+    {
+        public double Seguro { get; set; }
+
+        public Segurado(double seguro)
+        {
+            this.Seguro = seguro;
+        }
+
+        public double DoDesconto()
+        {
+            return base.DoTotal() * this.Seguro / 100;
+        }
+
+        public override double DoTotal()
+        {
+            return base.DoTotal() - DoDesconto();
+        }
+
+        public override string DoViewCupom()
+        {
+            return $"Subtotal: R${Math.Round(base.DoTotal(), 2)} // Desconto: R${Math.Round(DoDesconto(), 2)} // Total a Pagar: R${Math.Round(DoTotal(), 2)}";
+        }
+    }
+}
---
+++ 02 - FIAP_Console/AulasFIAP/03_Veiculos/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Veiculos.Models
+{
+    internal class Veiculo
+    {
+        public double ValorHora { get; set; }
+        public double ValorAdicional { get; set; }
+        public double Horas { get; set; }
+
+        public virtual double DoTotal()
+        {
+            return ValorHora + ValorAdicional * (Horas - 1);
+        }
+
+        public virtual string DoViewCupom()
+        {
+            return $"Total a pagar: {DoTotal()}";
+        }
+
+    }
+}
