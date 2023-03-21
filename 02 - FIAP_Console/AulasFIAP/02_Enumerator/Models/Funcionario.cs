4d76af883342b87ffb860c663b67be941935a22a
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-03-20 22:07:59 -0300

    Polimorfismo

---
+++ 02 - FIAP_Console/AulasFIAP/02_Enumerator/Models/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using _02_Enumerator.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Enumerator.Models
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public EstadoCivilEnum EstadoCivil { get; set; }
+
+        public Funcionario(string Nome)
+        {
+            this.Nome = Nome;
+        }
+    }
+}
--- 02 - FIAP_Console/AulasFIAP/02_Enumerator/Program.cs
+++ 02 - FIAP_Console/AulasFIAP/02_Enumerator/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace _02_Enumerator
+﻿using _02_Enumerator.Models;
+
+namespace _02_Enumerator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Funcionario func = new Funcionario("Fabio");
+            func.EstadoCivil = Enums.EstadoCivilEnum.Casado;
+
             
         }
     }
