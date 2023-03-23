7f2d05ae0c1f6a1b41cca7df71f957aed3849488
Fabio Sabariego Rodrigues <fabiosabariego87@hotmail.com>
2023-03-22 22:03:45 -0300

    Aula Listas

---
+++ 02 - FIAP_Console/AulasFIAP/01_Listas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace _01_Listas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var valores = new List<int> { -5, -2, -1, -10, 0, 15, 1, 2, 8, 7 };
+
+            List<int> filtro = valores.FindAll(element => element > 0);
+
+            Console.WriteLine(string.Join(", ", filtro));
+        }
+    }
+}
